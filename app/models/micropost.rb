85969de26063637d04291eae47142ac505e97984
David <david@Davids-MacBook-Pro.local>
2018-04-06 13:36:44 +0100

    added users and microposts

---
+++ app/models/micropost.rb
@@ -0,0 +1,5 @@
+class Micropost < ApplicationRecord
+  belongs_to :user
+  validates :content, length: { maximum: 140 }, presence: true
+  validates :user_id, length: { maximum: 140 }, presence: true
+end
