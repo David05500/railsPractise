cbd57ad461925828a73d4a48f7e48cfd9c3fdcc8
David <david.sharvashidze@mmtm.io>
2018-04-12 12:19:08 +0100

    got the basic pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    @hashy = { one: "uno", two: "dos", three: "tres" }
+
+    @params = [{ name: "Michael Hartl", email: "mhartl@example.com"}]
+    @params << { name: "Dom Chart", email: "dom@example.com" }
+    @params << { name: "Lara Barvin", email: "lara@example.com" }
+
   end
 
   def help
